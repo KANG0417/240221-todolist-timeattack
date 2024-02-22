af5e80d54675faecd6d7418a27ac27e34dd8d937
KANG0417 <ipia99@naver.com>
2024-02-22 19:14:10 +0900

    기초 세팅

--- src/App.js
+++ src/App.js
@@ -1,16 +1,5 @@
-import { useState } from "react";
-import TodoForm from "./components/TodoForm";
-import TodoList from "./components/TodoList";
-
 function App() {
-  const [todos, setTodos] = useState([]);
-
-  return (
-    <div className="App">
-      <TodoForm todos={todos} setTodos={setTodos} />
-      <TodoList todos={todos} />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
--- src/components/TodoList.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Todo from "./Todo";
-
-const TodoList = ({ todos }) => {
-  return <Todo todos={todos} />;
-};
-
-export default TodoList;
